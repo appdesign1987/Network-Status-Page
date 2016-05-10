8f64119bcf7a82610411dfcd77666dfe1eacc11b
appdesign1987 <jeroen@jeroenvd.nl>
2016-05-10 08:34:51 +0200

    Update functions.php

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -797,7 +797,7 @@ function makeWeatherSidebar()
 	$forecastUnits = '?units=si'; // Take a look at https://developer.forecast.io/docs/v2 to configure your weather information.
 	$forecastLang = '?lang=nl'; // Set summary language to NL
 	//$currentForecast = json_decode(file_get_contents('https://api.forecast.io/forecast/'.$forecast_api.'/'.$weather_lat.','.$weather_long.$forecastExcludes));
-	$currentForecast = json_decode(file_get_contents('https://api.forecast.io/forecast/'.$forecast_api.'/'.$weather_lat.','.$weather_long.$forecastExcludes.$forecastLang.$forecastUnits.));
+	$currentForecast = json_decode(file_get_contents('https://api.forecast.io/forecast/'.$forecast_api.'/'.$weather_lat.','.$weather_long.$forecastExcludes.$forecastLang.$forecastUnits));
 	$currentSummary = $currentForecast->currently->summary;
 	$currentSummaryIcon = $currentForecast->currently->icon;
 	$currentTemp = round($currentForecast->currently->temperature);
